59db537c9403dc673eacdc47ec624a32bd3523b4
roceil <a0978006326@gmail.com>
2024-02-22 09:41:17 +0800

    feat(server.js): 增加路由

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
-const http = require('http')
+const http = require('http');
+
+const user_list = [];
 
 const server_listener = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('Hello World\n')
-  res.end()
-}
+  router(req, res);
+};
 
-const server = http.createServer(server_listener)
+// switch 路由
+const router = (req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('歡迎光臨');
+    res.end();
+    return;
+  }
 
-server.listen(1234)
+  if (req.url === '/user' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(user_list));
+    res.end();
+    return;
+  } else if (req.url === '/user' && req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString(); // 將 Buffer 轉換成字符串
+    });
+    req.on('end', () => {
+      const userData = JSON.parse(body); // 現在可以解析 JSON 字符串
+      user_list.push(userData); // 將用戶資料添加到陣列中
+      res.writeHead(201, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(user_list));
+      res.end();
+    });
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify({ message: 'Not Found' }));
+  res.end();
+};
+
+const server = http.createServer(server_listener);
+
+server.listen(1234);
